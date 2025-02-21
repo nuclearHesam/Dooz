14d43c644b4a827841e1fd87b70b8344742fa9ba
Hesam <hesoyam.h.0913@gmail.com>
2025-02-21 13:37:46 +0330

    Create Game UI

--- Dooz/Form1.Designer.cs
+++ Dooz/Form1.Designer.cs
@@ -28,16 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
+            r2c2 = new Button();
+            r3c1 = new Button();
+            r3c2 = new Button();
+            r3c3 = new Button();
+            r2c1 = new Button();
+            r2c3 = new Button();
+            r1c2 = new Button();
+            r1c3 = new Button();
+            r1c1 = new Button();
             SuspendLayout();
+            // 
+            // r2c2
+            // 
+            r2c2.BackColor = SystemColors.Control;
+            r2c2.Font = new Font("Microsoft Sans Serif", 7.8F);
+            r2c2.Location = new Point(158, 162);
+            r2c2.Margin = new Padding(3, 4, 3, 4);
+            r2c2.Name = "r2c2";
+            r2c2.Size = new Size(140, 140);
+            r2c2.TabIndex = 18;
+            r2c2.UseVisualStyleBackColor = false;
+            // 
+            // r3c1
+            // 
+            r3c1.BackColor = SystemColors.Control;
+            r3c1.Font = new Font("Microsoft Sans Serif", 7.8F);
+            r3c1.Location = new Point(12, 310);
+            r3c1.Margin = new Padding(3, 4, 3, 4);
+            r3c1.Name = "r3c1";
+            r3c1.Size = new Size(140, 140);
+            r3c1.TabIndex = 17;
+            r3c1.UseVisualStyleBackColor = false;
+            // 
+            // r3c2
+            // 
+            r3c2.BackColor = SystemColors.Control;
+            r3c2.Font = new Font("Microsoft Sans Serif", 7.8F);
+            r3c2.Location = new Point(158, 310);
+            r3c2.Margin = new Padding(3, 4, 3, 4);
+            r3c2.Name = "r3c2";
+            r3c2.Size = new Size(140, 140);
+            r3c2.TabIndex = 16;
+            r3c2.UseVisualStyleBackColor = false;
+            // 
+            // r3c3
+            // 
+            r3c3.BackColor = SystemColors.Control;
+            r3c3.Font = new Font("Microsoft Sans Serif", 7.8F);
+            r3c3.Location = new Point(304, 310);
+            r3c3.Margin = new Padding(3, 4, 3, 4);
+            r3c3.Name = "r3c3";
+            r3c3.Size = new Size(140, 140);
+            r3c3.TabIndex = 15;
+            r3c3.UseVisualStyleBackColor = false;
+            // 
+            // r2c1
+            // 
+            r2c1.BackColor = SystemColors.Control;
+            r2c1.Font = new Font("Microsoft Sans Serif", 7.8F);
+            r2c1.Location = new Point(12, 162);
+            r2c1.Margin = new Padding(3, 4, 3, 4);
+            r2c1.Name = "r2c1";
+            r2c1.Size = new Size(140, 140);
+            r2c1.TabIndex = 14;
+            r2c1.UseVisualStyleBackColor = false;
+            // 
+            // r2c3
+            // 
+            r2c3.BackColor = SystemColors.Control;
+            r2c3.Font = new Font("Microsoft Sans Serif", 7.8F);
+            r2c3.Location = new Point(304, 162);
+            r2c3.Margin = new Padding(3, 4, 3, 4);
+            r2c3.Name = "r2c3";
+            r2c3.Size = new Size(140, 140);
+            r2c3.TabIndex = 13;
+            r2c3.UseVisualStyleBackColor = false;
+            // 
+            // r1c2
+            // 
+            r1c2.BackColor = SystemColors.Control;
+            r1c2.Font = new Font("Microsoft Sans Serif", 7.8F);
+            r1c2.Location = new Point(158, 14);
+            r1c2.Margin = new Padding(3, 4, 3, 4);
+            r1c2.Name = "r1c2";
+            r1c2.Size = new Size(140, 140);
+            r1c2.TabIndex = 12;
+            r1c2.UseVisualStyleBackColor = false;
+            // 
+            // r1c3
+            // 
+            r1c3.BackColor = SystemColors.Control;
+            r1c3.Font = new Font("Microsoft Sans Serif", 7.8F);
+            r1c3.Location = new Point(304, 14);
+            r1c3.Margin = new Padding(3, 4, 3, 4);
+            r1c3.Name = "r1c3";
+            r1c3.Size = new Size(140, 140);
+            r1c3.TabIndex = 11;
+            r1c3.UseVisualStyleBackColor = false;
+            // 
+            // r1c1
+            // 
+            r1c1.BackColor = SystemColors.Control;
+            r1c1.Font = new Font("Microsoft Sans Serif", 7.8F);
+            r1c1.Location = new Point(12, 14);
+            r1c1.Margin = new Padding(3, 4, 3, 4);
+            r1c1.Name = "r1c1";
+            r1c1.Size = new Size(140, 140);
+            r1c1.TabIndex = 10;
+            r1c1.UseVisualStyleBackColor = false;
             // 
             // GamePage
             // 
-            AutoScaleDimensions = new SizeF(11F, 24F);
+            AutoScaleDimensions = new SizeF(11F, 28F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaption;
-            ClientSize = new Size(471, 605);
-            Font = new Font("Roboto", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            Margin = new Padding(4);
+            ClientSize = new Size(457, 465);
+            Controls.Add(r2c2);
+            Controls.Add(r3c1);
+            Controls.Add(r3c2);
+            Controls.Add(r3c3);
+            Controls.Add(r2c1);
+            Controls.Add(r2c3);
+            Controls.Add(r1c2);
+            Controls.Add(r1c3);
+            Controls.Add(r1c1);
+            Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(4, 5, 4, 5);
+            MaximizeBox = false;
+            MinimizeBox = false;
             Name = "GamePage";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Dooz";
@@ -45,5 +165,15 @@
         }
 
         #endregion
+
+        private Button r2c2;
+        private Button r3c1;
+        private Button r3c2;
+        private Button r3c3;
+        private Button r2c1;
+        private Button r2c3;
+        private Button r1c2;
+        private Button r1c3;
+        private Button r1c1;
     }
 }
